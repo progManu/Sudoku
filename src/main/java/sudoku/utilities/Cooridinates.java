5f3f59f53a1b7b13cbdae72f86425eadbc2d41f2
progManu <41389275+progManu@users.noreply.github.com>
2023-09-07 11:03:09 +0200

    Adding Coordinates class

---
+++ src/main/java/sudoku/utilities/Cooridinates.java
@@ -0,0 +1,35 @@
+package sudoku.utilities;
+
+public class Cooridinates {
+    private int x, y;
+
+    public Cooridinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(obj.getClass() != this.getClass()) return false;
+        Cooridinates temp = (Cooridinates)  obj;
+        return (this.x == temp.x) &&
+                (this.y == temp.y);
+    }
+}
